6d7f9849365b38490f4fcf49b5ca6f2ca64de649
owwwl666 <zaharsalam228@gmail.com>
2023-07-19 22:41:55 +1000

    Created model Client
    Created a model and configured it in the admin panel.

--- cake_app/models.py
+++ cake_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    """"""
+    client_tg_id = models.IntegerField(verbose_name="ID пользователя")
+    client_tg_username = models.CharField(max_length=200, verbose_name="Имя пользователя")
+
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+
+class Order(models.Model):
+    """"""
+    client = models.ForeignKey(to='cake_app.Client', on_delete=models.CASCADE,
+                               related_name="orders")
+    address = models.TextField(verbose_name="Адресс заказа")
+    delivery_date = models.DateField(verbose_name="Дата доставки")
+    delivery_time = models.TimeField(verbose_name="Время доставки")
+    promocode = models.CharField(max_length=50, blank=True, null=True,
+                                 verbose_name="Промокод")
+
+    # comment = models.TimeField(blank=True, null=True, verbose_name="Комментарий к заказу")
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
